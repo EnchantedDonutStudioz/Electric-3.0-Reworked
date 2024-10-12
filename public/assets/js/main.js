36458c2166de7f013e79ed5d38e341f677785405
EnchantedDonutStudioz <EnchantedDonutStudioz@gmail.com>
2024-10-11 20:45:24 -0400

    sdfsdfds

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,8 +5,9 @@ const input = document.getElementById("i");
 async function init() {
     try {
         const connection = new BareMux.BareMuxConnection("/baremux/worker.js")
+        let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
         if (await connection.getTransport() !== "/epoxy/index.mjs") {
-            await connection.setTransport("/epoxy/index.mjs", [{}]);
+            await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
             console.log("Using websocket transport");
         }
     } catch (error) {
